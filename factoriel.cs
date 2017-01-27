0149142e680526a1d2aae8f91b38146a22529852
viktorMirev <viktor.mirev@gmail.com>
2017-01-27 20:32:54 +0200

    Add files via upload

---
+++ factoriel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace HomeWo
+{
+    class Program
+    {
+       public static void Main()
+        {
+            Factoriel(int.Parse(Console.ReadLine()));
+        }
+
+        private static void Factoriel(int n)
+        {
+            if (n == 0) Console.WriteLine("1");
+            BigInteger prev = 1;
+            for (int    curr=1; curr<=n; curr++)
+            {
+                prev = prev*curr;
+            }
+            Console.WriteLine(prev);
+        }
+    }
+}
---
+++ factorielTrailingZeros.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace HomeWo
+{
+  
+    class Program
+    {
+         static int count=0;
+       public static void Main()
+        {
+            TrZeros(Factoriel(int.Parse(Console.ReadLine())));
+        }
+       
+        private static void TrZeros(BigInteger bigInteger)
+        {
+            if (bigInteger % 10 == 0) { count++; bigInteger = bigInteger / 10; TrZeros(bigInteger); }
+            else Console.WriteLine(count);
+
+        }
+            
+        private static BigInteger Factoriel(int n)
+        {
+            if (n == 0) Console.WriteLine("1");
+            BigInteger prev = 1;
+            for (int    curr=1; curr<=n; curr++)
+            {
+                prev = prev*curr;
+            }
+            //Console.WriteLine(prev);
+            return prev;
+        }
+    }
+}
